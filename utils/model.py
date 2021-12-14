1d3b8c3da85c97973a044225f1954b13926b070c
Charlie <charliesharrington@gmail.com>
2021-12-13 21:47:01 -0500

    Adding save and load object functions

--- utils/model.py
+++ utils/model.py
@@ -2,6 +2,7 @@
 Model utilities
 '''
 import torch
+import pickle
 
 
 # vectorize model
@@ -15,3 +16,15 @@ def try_gpu(i=0):
     if torch.cuda.device_count() >= i + 1:
         return torch.device(f"cuda:{i}")
     return torch.device("cpu")
+
+
+# save object
+def save_obj(obj, name):
+    file = open(f"checkpoints/{name}.obj", "wb")
+    pickle.dump(obj, file)
+
+
+# load object
+def load_obj(name):
+    file = open(f"checkpoints/{name}.obj", "rb")
+    return pickle.load(file)
